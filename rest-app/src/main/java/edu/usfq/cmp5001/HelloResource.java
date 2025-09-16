e8b7c70db488065ff46e29f7e32992cc1da9e425
Joel Cuascota <jcuascota@estud.usfq.edu.ec>
2025-09-15 20:42:36 -0500

    Deber 1: JSF + REST con Docker

---
+++ rest-app/src/main/java/edu/usfq/cmp5001/HelloResource.java
@@ -0,0 +1,15 @@
+package edu.usfq.cmp5001;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+@Path("/hello")
+public class HelloResource {
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello() {
+        return "Hello from REST";
+    }
+}
---
+++ rest-app/src/main/java/edu/usfq/cmp5001/JaxrsActivator.java
@@ -0,0 +1,7 @@
+package edu.usfq.cmp5001;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class JaxrsActivator extends Application { }
